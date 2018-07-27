055c0a6c916d3a5151a380f04490b313a9693526
Heo-H <h0h6h2h5@gmail.com>
2018-07-27 11:42:10 +0900

    Solve by filling array

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# [문제 출처]()
-## [문제 이름]()
+# [SWEA](https://www.swexpertacademy.com/)
+## [1961. 숫자 배열 회전](https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Pq-OKAVYDFAUq)
+
+* 풀이 시간:
+  1. 2018-07-27 11:20:05 ~ 2018-07-27 11:36:41
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,58 @@
+#include <cstdio>
+
+int mat[4][7][7];
+int n;
+
+
+void rotate90(int src[7][7], int dst[7][7]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            dst[i][j] = src[n-1-j][i];
+        }
+    }
+}
+
+void fillAll() {
+    for (int m = 1; m < 4; m++) {
+        rotate90(mat[m-1], mat[m]);
+    }
+}
+
+void printAll() {
+    for (int i = 0; i < n; i++) {
+        for (int m = 1; m < 4; m++) {
+            for (int j = 0; j < n; j++) {
+                printf("%d", mat[m][i][j]);
+            }
+            if (m < 3) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void solve() {
+    fillAll();
+    printAll();
+}
 
 int main() {
+    int t;
+    scanf("%d", &t);
+
+    for (int tc = 1; tc <= t; tc++) {
+        scanf("%d", &n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                scanf("%d", &mat[0][i][j]);
+            }
+        }
+
+        printf("#%d\n", tc);
+        solve();
+    }
+
     return 0;
 }
