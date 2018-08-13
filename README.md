dcac6d2c1372ebc2b23e63d096abe25b05a2d4bf
Heo-H <h0h6h2h5@gmail.com>
2018-08-13 14:56:47 +0900

    Solve by mod, div operators

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# [문제 출처]()
-## [문제 이름]()
+# [SWEA](https://www.swexpertacademy.com/)
+## [1970. 쉬운 거스름돈](https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PsIl6AXIDFAUq)
+
+* 풀이 시간:
+  1. 2018-08-13 14:47:09 ~ 2018-08-13 14:55:44
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,36 @@
+﻿#include <cstdio>
+
+const int NUM_OF_MONEYS = 8;
+const int MONEY[] = { 50000, 10000, 5000, 1000, 500, 100, 50, 10 };
+
+void solve(int counts[NUM_OF_MONEYS], int n) {
+    for (int i = 0; i < NUM_OF_MONEYS; i++) {
+        counts[i] = n / MONEY[i];
+        n %= MONEY[i];
+    }
+}
+
+void printArray(int counts[NUM_OF_MONEYS]) {
+    for (int i = 0; i < NUM_OF_MONEYS; i++) {
+        printf("%d ", counts[i]);
+    }
+}
 
 int main() {
+    int T;
+    scanf("%d", &T);
+
+    for (int tc = 1; tc <= T; tc++) {
+        int n;
+        scanf("%d", &n);
+
+        int counts[NUM_OF_MONEYS];
+        solve(counts, n);
+
+        printf("#%d\n", tc);
+        printArray(counts);
+        printf("\n");
+    }
+
     return 0;
 }
