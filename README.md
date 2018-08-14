679f9406b828747f64cc9bc5117e8240c192c33c
Heo-H <h0h6h2h5@gmail.com>
2018-08-14 22:22:58 +0900

    Fail by timeout

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# [문제 출처]()
-## [문제 이름]()
+# [SWEA](https://www.swexpertacademy.com/)
+## [[Battle 30] No. 3 아름다운 균일 수](https://swexpertacademy.com/main/talk/codeBattle/problemDetail.do?contestProbId=AWUi4oN6pGgDFAVT&categoryId=AWUW18-KzA0DFAVT&categoryType=BATTLE)
+
+* 풀이 시간:
+  1. 2018-08-14 20:25:23 ~ 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,63 @@
+#include <cstdio>
+#include <cmath>
+
+long long count(int n, int b) {
+    long long cnt = 0;
+
+    int i = 2;
+
+    int log = 2;
+    long long pow = (long long) b * b;
+    while (pow <= n) {
+        pow *= b;
+        log++;
+    }
+
+
+    long long unit = (1 - pow) / (1 - b);
+
+    if (log < 2) {
+        return 0;
+    }
+
+    cnt += (long long)(b - 1) * (log - 2);
+    long long val = unit;
+    for (int j = 1; j < b; j++) {
+        if (val <= n) {
+            cnt++;
+            val += unit;
+        }
+        else {
+            break;
+        }
+    }
+
+    return cnt;
+}
+
+long long solve(int n) {
+    long long sum = 0;
+
+    for (int b = 2; b < n; b++) {
+        sum += (long long)b * count(n, b);
+    }
+
+    return sum;
+}
 
 int main() {
+    //setbuf(stdout, NULL);
+
+    int T;
+    scanf("%d", &T);
+
+    for (int tc = 1; tc <= T; tc++) {
+        int N;
+        scanf("%d", &N);
+
+        long long answer = solve(N);
+        printf("#%d %lld\n", tc, answer);
+    }
+
     return 0;
 }
