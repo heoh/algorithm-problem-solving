f25b642bbf47e06aa4a18e06de8df20efadc2c0b
Heo-H <h0h6h2h5@gmail.com>
2018-09-09 16:54:20 +0900

    Time Limit Exceed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# [문제 출처]()
-## [문제 이름]()
+# [SWEA](https://www.swexpertacademy.com/)
+## [5363. 부분 수열의 힘](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWVWljaKsZkDFAUO)
+
+* 풀이 시간:
+  1. 2018-08-27 20:26:37 ~ 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,93 @@
+#include <cstdio>
+#include <cstring>
+
+
+int map[9][9];
+
+
+bool checkRow(int i) {
+    bool contains[10];
+    memset(contains, false, sizeof(contains));
+
+    for (int j = 0; j < 9; j++) {
+        int value = map[i][j];
+        if (contains[value]) {
+            return false;
+        }
+        contains[value] = true;
+    }
+    return true;
+}
+
+bool checkCol(int j) {
+    bool contains[10];
+    memset(contains, false, sizeof(contains));
+
+    for (int i = 0; i < 9; i++) {
+        int value = map[i][j];
+        if (contains[value]) {
+            return false;
+        }
+        contains[value] = true;
+    }
+    return true;
+}
+
+bool checkSquare(int r, int c) {
+    bool contains[10];
+    memset(contains, false, sizeof(contains));
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            int value = map[r + i][c + j];
+            if (contains[value]) {
+                return false;
+            }
+            contains[value] = true;
+        }
+    }
+    return true;
+}
+
+bool checkValid() {
+    for (int i = 0; i < 9; i++) {
+        if (!checkRow(i)) {
+            return false;
+        }
+    }
+    for (int i = 0; i < 9; i++) {
+        if (!checkCol(i)) {
+            return false;
+        }
+    }
+    for (int i = 0; i < 9; i += 3) {
+        for (int j = 0; j < 9; j += 3) {
+            if (!checkSquare(i, j)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 int main() {
+    int T;
+    scanf("%d", &T);
+
+    for (int tc = 1; tc <= T; tc++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                scanf("%d", &map[i][j]);
+            }
+        }
+
+        if (checkValid()) {
+            printf("#%d %d\n", tc, 1);
+        }
+        else {
+            printf("#%d %d\n", tc, 0);
+        }
+    }
+
     return 0;
 }
